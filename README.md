49a7082e93d8404e937a8b5b870f96774637133d
Stoanb <Stian.ngo@lyse.net>
2021-11-25 12:18:19 +0100

    Added deployment phase - no functionality yet

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Santorini
 EksamensinleveringOppgave for MM 200
 
+#Når men trykker registrer det array info , hvor brikken er på arrayen.
+#legge ut begge brikkene
+#skjekkliste med logikk 
--- main.js
+++ main.js
@@ -1,52 +1,113 @@
 let htmlTable = document.getElementById("SantoTable");
 
 let p1a = {
-    pos: playerposArr,
-    floorlevel: test,
-};
+    
+    };
 
 let p1b = {
-    pos: playerposArr,
-    floorlevel: test,
+       
 };
 
-console.log(playerposArr[3][2]);
-
-
-/*let playerposArr = [
+let playerposArr = [
     [0,0,0,0,0],
     [0,0,0,0,0],
     [0,0,0,0,0],
     [0,0,0,0,0],
     [0,0,0,0,0]
-]; */
-
+]; 
+// Må ha to, enten må kun playeren stå i arrayen eller hva nivå bygningen er på 
 let mapArr = [
     [0,0,0,0,0],
     [0,0,0,0,0],
-    [0,0,1,2,0],
+    [0,0,0,0,0],
     [0,0,0,0,0],
     [0,0,0,0,0]
 ];
 
-let deploy = true; 
+let deploymentphase = true; 
+let deployCount = 0;
 
+let isItPlayer1Turn = true;
 
+let currentCell;
+
+function switchTurns(){
+    isItPlayer1Turn = !isItPlayer1Turn;
+};
 
 const tbody = document.querySelector('#SantoTable tbody');
 tbody.addEventListener('click', function (e){
   const cell = e.target.closest('td');
+  currentCell = cell;
+  
   if (!cell) {
     return;
     }
-    else{
+
+    if(deploymentphase && isItPlayer1Turn && cell.innerHTML === "" && deployCount < 2){
+
+        const placeHolderPlayer1 = `P1-${deployCount}`;
+        console.log("Player 1 turn");
+
+        updateArr(cell.id, placeHolderPlayer1);
+        updatePlayingBoard(placeHolderPlayer1);
+
+        deployCount ++;
+        if (deployCount === 2){
+            switchTurns();
+            deployCount = 0;
+        }
         
-        updateArr(cell.id);
+    }
+
+    if(deploymentphase && !isItPlayer1Turn && cell.innerHTML === "" && deployCount < 2){
+      
+
+        const placeHolderPlayer2 = `P2-${deployCount}`;
+        console.log("Player 1 turn");
+
+        updateArr(cell.id, placeHolderPlayer2);
+        updatePlayingBoard(placeHolderPlayer2);
+
+        deployCount ++
+        
+        if (deployCount === 2){
+            switchTurns();
+            console.log("Deployment phase over")
+            deploymentphase = false;
+        }
+    }
+
+    if (isItPlayer1Turn && cell.innerHTML === ""){
+        const placeHolderPlayer1 = "P1a";
+        console.log("Player 1 turn");
+
+        updateArr(cell.id, placeHolderPlayer1);
+        updatePlayingBoard(placeHolderPlayer1);
+
+        switchTurns();
+        
+    }
+    else if(!isItPlayer1Turn && cell.innerHTML === ""){
+        const placeHolderPlayer2 = "P2a";
+        console.log("Player 2 turn");
+
+        updateArr(cell.id, placeHolderPlayer2);
+        updatePlayingBoard(placeHolderPlayer2);
+
+        switchTurns();
     }
 });
 
-function updateArr (cellid){
+function updateArr (cellId, cellText){
 
-    console.log(cellid);
+    playerposArr[0][cellId] = cellText;
+    console.log(playerposArr);
+
+}
+
+function updatePlayingBoard(cellText){
+
+    currentCell.innerHTML = cellText;
 
 }
